907c8ecfa14f700ed0b2c8b885d5843cd9146f7d
mosabbir <mosabbirhossain19@gmail.com>
2020-12-18 12:12:24 +0600

    commited by mosabbir

---
+++ Final First Lab/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_First_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Savings("Nayamet Ullah", "A101", 5000,5);
+            a1.ShowInfo();
+            a1.Deposit(5000);
+            a1.Withdraw(9700);
+            a1.ShowInfo();
+            Console.WriteLine();
+           
+            Account a2 = new Fixed("Anik", "A201", 5000,5,2015);
+            a2.ShowInfo();
+            a2.Deposit(5000);
+            a2.Withdraw(9700);
+            a2.ShowInfo();
+            Console.WriteLine();
+
+            Account a3 = new SpecialSaving("Fahim", "A301", 2000, 5);
+            a3.ShowInfo();
+            a3.Deposit(5000);
+            a3.Withdraw(6600);
+            a3.ShowInfo();
+            Console.WriteLine();
+            Account a4 = new OverDraft("Refayet", "A401", 3000, 1000);
+            a4.ShowInfo();
+            a4.Transfer(2000, a1);
+            a4.ShowInfo();
+            
+
+        }
+    }
+}
---
+++ GUI Practice/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            string mobile = textBoxMobile.Text;
+            string orgName = textBoxOrgNmae.Text;
+            string orgAddress = textBoxOrgAddress.Text;
+            string output = String.Format("Name: {0}\nMobile: {1}\nOrg Name: {2}\nOrg Address:{3}", name, mobile, orgName, orgAddress);
+            richTextOutput.Text = output;
+        }
+    }
+}
